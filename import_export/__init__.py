e5aa7d4fdb7b56c39f0dccd30e3c5a3827b41a62
Kalila <69767640+digisomni@users.noreply.github.com>
2024-10-23 13:54:40 +1100

    Fix import export operator issue [restore missing file].

--- import_export/__init__.py
+++ import_export/__init__.py
@@ -2,15 +2,17 @@ from . import old_json_importer
 from . import old_json_exporter
 from . import world_import
 from . import old_gltf_exporter
-
+from . import old_gltf_importer
 def register():
     old_json_importer.register()
     old_json_exporter.register()
     world_import.register()
+    old_gltf_importer.register()
     old_gltf_exporter.register()
 
 def unregister():
     old_gltf_exporter.unregister()
+    old_gltf_importer.unregister()
     world_import.unregister()
     old_json_exporter.unregister()
     old_json_importer.unregister()
--- operators/import_operators.py
+++ operators/import_operators.py
@@ -3,7 +3,8 @@ import os
 from bpy_extras.io_utils import ImportHelper
 from bpy.props import CollectionProperty, StringProperty
 from bpy.types import Operator
-from ..import_export import old_json_importer, world_import
+
+from ..import_export import old_gltf_importer
 from ..ui.tooltips import ImportExportTooltips
 
 class IMPORT_OT_vircadia_json(bpy.types.Operator):
@@ -41,7 +42,7 @@ class IMPORT_OT_vircadia_gltf(Operator, ImportHelper):
 
     def execute(self, context):
         filepaths = [os.path.join(self.directory, name.name) for name in self.files]
-        world_import.import_gltf_or_glb(context, filepaths)
+        old_gltf_importer.import_gltf_or_glb(context, filepaths)
         return {'FINISHED'}
 
 def register():
